05df1480accdc612dfea218fae0fa5d2eb877680
alphonsa <alphonsanew22@gmail.com>
2023-06-21 12:27:40 +0530

    updated

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -9,21 +9,42 @@ import SwiftUI
 import MyExamplePackage
 
 struct ContentView: View {
+    @State private var showToast: Bool = false
+    @State private var toastMessage: String = ""
+
     var body: some View {
         VStack {
-                  Button("Show Log") {
-                      Logger.log("This is a debug message", level: .debug)
-                  }
-              }
-              .padding()
-              .onAppear {
-                  Logger.logLevel = .debug
-              }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            Button("Show Log") {
+                Logger.log("This is a debug message", level: .debug)
+            }
+        }
+        .padding()
+        .onAppear {
+            Logger.logLevel = .debug
+            NotificationCenter.default.addObserver(forName: NSNotification.Name("ShowToast"), object: nil, queue: nil) { notification in
+                if let logMessage = notification.object as? String {
+                    showToast = true
+                    toastMessage = logMessage
+                }
+            }
+            NotificationCenter.default.addObserver(forName: NSNotification.Name("DismissToast"), object: nil, queue: nil) { _ in
+                showToast = false
+                toastMessage = ""
+            }
+        }
+        .overlay(
+            Group {
+                if showToast {
+                    ToastView(message: toastMessage)
+                        .transition(.opacity)
+                        .animation(.easeInOut)
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                NotificationCenter.default.post(name: NSNotification.Name("DismissToast"), object: nil)
+                            }
+                        }
+                }
+            }
+        )
     }
 }
--- Sources/MyExamplePackage/Toaster.swift
+++ Sources/MyExamplePackage/Toaster.swift
@@ -9,19 +9,13 @@ import SwiftUI
 
 struct ToasterView: View {
     let message: String
-       
-       var body: some View {
-           Text(message)
-               .foregroundColor(.white)
-               .padding()
-               .background(Color.black.opacity(0.7))
-               .cornerRadius(10)
-               .onAppear {
-                   DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                       // Remove the toast view after 2 seconds
-                       NotificationCenter.default.post(name: NSNotification.Name("DismissToast"), object: nil)
-                   }
-               }
-       }
+    
+    var body: some View {
+        Text(message)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.black.opacity(0.7))
+            .cornerRadius(10)
+    }
 }
 
